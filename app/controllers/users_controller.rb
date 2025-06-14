8ee28cf0f481b5c181af19c2754d7493d4670c5c
samaraximenes <ximenesam@gmail.com>
2025-06-14 19:18:06 -0300

    Correção de filtros

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
   def index
-    users = User
-              .by_company(params[:company_identifier])
-              .by_username(search_params[:username])
+    users = User.by_company(params[:company_id])
+                .by_username(search_params[:username])
+                
     render json: users.all
   end
 
